f5bb7b3549b97d344485c42a6810e47a74162925
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-10-25 21:22:53 +0200

    Update run_models.py

--- scripts/run_models.py
+++ scripts/run_models.py
@@ -73,7 +73,7 @@ def get_model_and_data(offset=0, num_models=-1):
             try:
                 model_name = p.posterior_info["model_name"]
                 data_name = p.posterior_info["data_name"]
-                model_data = f"{model_name}_{data_name}"
+                model_data = f"{data_name}_{model_name}"
                 try:
                     model_code = Path(DB.get_model_code_path(model_name, "stan"))
                 except FileNotFoundError:
@@ -125,7 +125,7 @@ def run(offset=0, num_models=-1):
         if "FAIL_INFO" in information:
             break
         try:
-            model_name = f'{information["model_name"]}_{information["data_name"]}'
+            model_name = f'{information["data_name"]}_{information["model_name"]}'
             start_build_model = time()
             model = cmdstanpy.CmdStanModel(
                 model_name=model_name, stan_file=information["model_code"]
@@ -148,7 +148,7 @@ def run(offset=0, num_models=-1):
             else:
                 fit = model.sample(
                     data=str(information["data"]),
-                    chains=8,
+                    chains=6,
                     seed=42,
                     iter_warmup=500,
                     iter_sampling=500,
